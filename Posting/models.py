7f867341f8a825b9b6b87a42a2873dc5e5e5bf3e
haleykimm <accidentalgap@gmail.com>
2022-10-14 10:27:22 +0900

    dfsd

--- Posting/models.py
+++ Posting/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Post(models.Model):
-    author = models.ForeignKey(to=User, related_name="user_posts", verbose_name="작성자", on_delete=models.CASCADE, max_length=10, db_column="author")
+    author = models.ForeignKey(to=User, related_name="user_posts", verbose_name="작성자", on_delete=models.CASCADE, max_length=10)
     title = models.CharField(max_length=50)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
